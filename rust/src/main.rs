89a6d4e183d6b62517786feb40f14023a662b4f6
thrombe <yash.n.thombare620@gmail.com>
2022-04-29 14:47:54 +0530

    added basic structure for rust version

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,54 @@
+
+use rand;
+
+struct GuessProvider {
+
+}
+
+impl Default for GuessProvider {
+    fn default() -> Self {
+        todo!()
+    }
+}
+
+
+impl GuessProvider {
+    fn provide_guess() -> u32 {
+        todo!()
+    }
+}
+
+struct InputHandler {
+    
+}
+
+impl Default for InputHandler {
+    fn default() -> Self {
+        todo!()
+    }
+}
+
+impl InputHandler {
+    fn provide_guess() -> u32 {
+        todo!()
+    }
+}
+
 fn main() {
-    println!("Hello World!!");
+
+    let rng = GuessProvider::default();
+    let input = InputHandler::default();
+    loop {
+        todo!()
+        let computer_guess = rng.provide_guess();
+        let player_guess = input.provide_guess();
+        if player_guess == computer_guess {
+            println!("You have successfully guessed the number");
+            break;
+        } else if player_guess > computer_guess {
+            println!("Guess is Higher than N.");
+        } else {
+            println!("Guess is Lower than N.");
+        }
+    }
 }
